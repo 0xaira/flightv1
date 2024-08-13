6bc1a069d09b8017b3c08cc8b07436491c5d3b89
Aira Jena <airajena.build@gmail.com>
2024-08-13 11:21:49 +0530

    feat: Implement update functionality for airplanes

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -34,12 +34,16 @@ class CrudRepository {
         return response;
     }
 
-    async update(id, data) {
+    async update(id, data){
         const response = await this.model.update(data, {
             where: {
                 id: id
             }
-        })
+        });
+        if(response[0] == 0) {
+            throw new AppError("Not able to find the resource", StatusCodes.NOT_FOUND);
+        }
+        
         return response;
     }
 }
