e9b837a7066d6819f6062fa1b4afb3189a6c66c0
Aira Jena <airajena.build@gmail.com>
2024-08-16 12:59:17 +0530

    feat: Add Flights model with associations and foreign key constraints

--- src/models/airport.js
+++ src/models/airport.js
@@ -14,8 +14,16 @@ module.exports = (sequelize, DataTypes) => {
       this.belongsTo(models.City, {
         foreignKey: 'cityId'
       });
-    }
+    this.hasMany(models.Flight, {
+      foreignKey: 'departureAirportId',
+      onDelete: 'CASCADE'
+    });
+    this.hasMany(models.Flight, {
+      foreignKey: 'arrivalAirportId',
+      onDelete: 'CASCADE'
+    });
   }
+}
   Airport.init({
     name: {
       type: DataTypes.STRING,
